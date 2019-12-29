072663ca11ae0948256c13cb69e4a149b1f88d06
lxj <kimlbb@163.com>
2019-12-29 14:06:22 +0800

    数据库及操作说明

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# kjbg
-快速测试
+
+##启动配置
+windows下的host配置  
+127.0.0.1 auth-service  
+127.0.0.1 config  
+127.0.0.1 registry  
+127.0.0.1 gateway  
+
+##idea插件
+lombok  
+
+# 测试登录
+POST http://gateway:4000/system/login/in?username=root&password=123456
+
+
+
+
+
---
+++ data.sql
@@ -0,0 +1,95 @@
+/*
+SQLyog Enterprise Trial - MySQL GUI v7.11 
+MySQL - 8.0.17 : Database - cloud-auth
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`cloud-auth` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `cloud-auth`;
+
+/*Table structure for table `permission` */
+
+DROP TABLE IF EXISTS `permission`;
+
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `ename` varchar(50) DEFAULT NULL,
+  `url` varchar(500) DEFAULT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `permission` */
+
+insert  into `permission`(`id`,`parent_id`,`name`,`ename`,`url`,`created`) values (1,0,'所有权限','admin','/**',NULL);
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `role` */
+
+insert  into `role`(`id`,`name`,`created`) values (1,'超级管理员','0000-00-00 00:00:00');
+
+/*Table structure for table `role_permission_relation` */
+
+DROP TABLE IF EXISTS `role_permission_relation`;
+
+CREATE TABLE `role_permission_relation` (
+  `role_id` int(11) DEFAULT NULL,
+  `per_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `role_permission_relation` */
+
+insert  into `role_permission_relation`(`role_id`,`per_id`) values (1,1);
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `name` varchar(100) NOT NULL COMMENT '昵称',
+  `mobile` varchar(100) NOT NULL COMMENT '手机号',
+  `mail` varchar(100) NOT NULL COMMENT '邮箱',
+  `pwd` varchar(100) NOT NULL COMMENT '密码',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`name`,`mobile`,`mail`,`pwd`) values (1,'root','13555555555','10086@qq.com','$2a$10$hcMi5tIUGGGim/Xe0Z7q4e5Zz3QlK.EAek3an3nZf0B.ZdN0GJgSe');
+
+/*Table structure for table `user_role_relation` */
+
+DROP TABLE IF EXISTS `user_role_relation`;
+
+CREATE TABLE `user_role_relation` (
+  `user_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `user_role_relation` */
+
+insert  into `user_role_relation`(`user_id`,`role_id`) values (1,1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
